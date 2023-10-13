1a6070334de0616b7d91306ce3f3e24278c73d72
Nidhi Mishra <113460614+Nidhiiiim@users.noreply.github.com>
2023-10-13 13:02:38 -0400

    Loops and Arrays

--- Array_Basic.py
+++ Array_Basic.py
@@ -4,9 +4,21 @@ from array import *
 # Declared an empty array
 arr = array("i", [])
 x = int(input("Please provide the size of 2 D Array: "))
-print("Size of your 2D array is %d" %x)
+print("Size of your 2D array is %d" % x)
 
 for i in range(x):
     ele = int(input())
-    arr.append(ele) # Pushing element to array
+    arr.append(ele)  # Pushing element to array
+print(arr)
+
+# Removing Duplicates in input array
+i = 0
+while i < x - 1:
+    j = i + 1
+    while j < x:
+        if arr[i] == arr[j]:
+            del arr[j]
+            x = x - 1
+        j += 1
+    i += 1
 print(arr)

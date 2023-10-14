56fff85b083e7478f4ac54d4eed87591da745b26
Nidhi Mishra <113460614+Nidhiiiim@users.noreply.github.com>
2023-10-13 21:43:13 -0400

    Dictionary Updated

--- Set & Dictionary.py
+++ Set & Dictionary.py
@@ -1,2 +1,18 @@
 # Dictionary is unordered key value pairs
+# Keys MUST be Unique
+month_map = {
+    "Jan":"January",
+    "Feb":"February",
+    "Mar":"March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sept": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
 
+print(month_map.get("Dec"))
